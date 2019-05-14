827b66d0b1f56cd0c3f5da3d003ce0662e1a52f5
lucaspompeun <lucaspompeuneves@gmail.com>
2019-05-14 16:04:24 -0300

    commit

--- main.py
+++ main.py
@@ -1,2 +1,7 @@
 from Bio import Entrez, SeqIO
 import os
+
+# read fastQ
+def readFastq(filename):
+    with open(filename) as fastq:
+        
---
+++ test.py
@@ -0,0 +1,5 @@
+import collections 
+
+x = 'atatccgatcbgtcatgcgtgtacnnattctan'
+
+print(collections.Counter(x))
