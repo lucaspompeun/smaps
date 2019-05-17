bbeb7a71a33d4789aca98e1effe16b6cca9e8578
lucaspompeun <lucaspompeuneves@gmail.com>
2019-05-17 15:31:29 -0300

    inclusao das funcoes de blast e falta verificar fluxograma pro entrez

--- blast.py
+++ blast.py
@@ -2,22 +2,37 @@ from Bio import SeqIO
 from Bio.Blast import NCBIWWW
 import sys, os
 
-# blast de nucleotideos com o NCBI
+# blast de nucleotideos com o NCBI para um contig
 def blastn(filename):
     query = SeqIO.read(filename, format="fasta") 
     blast = NCBIWWW.qblast("blastn", "nt", query.seq)
-    blastFile = open("my_blast.xml", "w") 
+    blastFile = open("blastn.xml", "w") 
     blastFile.write(blast.read())
     blastFile.close()
     blast.close()
 
-def blastx(query):
-    cmd = 'blastx -query ' + str(query) + '-db nt -out result.xml -evalue 0.001 -outfmt 6'
+    return
+
+
+# blast de nucleotideos com o NCBI para varios contigs
+def multiblastn(filename):
+    for record in SeqIO.parse(filename, "fasta"):
+        blast = NCBIWWW.qblast('blastn', 'nt', record.seq)
+        blastFile = open(record.id + '-blastn.xml', 'w')
+        blastFile.write(blast.read())
+        blastFile.close()
+        blast.close()
+
+    return
+
+
+# blast local de 2 sequencias que tem o tabular 6 no output
+def blastnlocal6(query, subject):
+    out = 'blastn.outfmt6'
+    cmd = 'blastn -query ' + str(query) + ' -subject ' + str(subject) + ' -outfmt 6 > ' + out
     os.system(cmd)
 
+    return
 
 
-
-#blastn(sys.argv[1])
-blastx(sys.argv[1])
-
+#### funcao para buscar as sequencias pelo entrez no NCBI
