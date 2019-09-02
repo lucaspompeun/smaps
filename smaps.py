250ea97a5df76801d2ce6d46483e1d19af713c01
Lucas Pompeu <lucaspompeuneves@gmail.com>
2019-09-02 19:25:44 +0000

    added reference to argparse

--- smaps.py
+++ smaps.py
@@ -5,6 +5,7 @@ parser = argparse.ArgumentParser(description = 'Smaps - A tool to extends contig
 
 parser.add_argument('-read1', action='store', dest='read1', required = True, help='first fastq file (required)')
 parser.add_argument('-read2', action='store', dest='read2', required = True, help='second fastq file (required)')
+parser.add_argument('-reference', action='store', dest='reference', required = False, default = None, help='reference genome (optional)')
 parser.add_argument('-project', action='store', dest='project', required = True, help='name of project (required)')
 parser.add_argument('-o', action='store', dest='o', type = int, default = 5, required = False, help='minimum number of reads needed to extend a base (optional)')
 parser.add_argument('--version', action='version', version='Smaps 1.0')
@@ -14,5 +15,6 @@ read1 = results.read1
 read2 = results.read2
 project = results.project
 o = results.o
+reference = results.reference
 
-main(read1, read2, project, o)
+main(read1, read2, project, o, reference)
