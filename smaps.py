05163a7be6bd203d97465cfd1a4e13378e1bdc15
lucaspompeun <lucaspompeuneves@gmail.com>
2019-11-26 16:02:53 -0300

    atualização chamada na linha de comando

--- smaps.py
+++ smaps.py
@@ -12,6 +12,8 @@ parser.add_argument('-read2', action='store', dest='read2',
                     required=False, default=None, help='second fastq file (optional)')
 parser.add_argument('-reference', action='store', dest='reference',
                     required=False, default=None, help='reference genome (optional)')
+parser.add_argument('-gff', action='store', dest='gff',
+                    required=False, default=None, help='gff (optional)')
 parser.add_argument('-project', action='store', dest='project',
                     required=True, help='name of project (required)')
 parser.add_argument('-o', action='store', dest='o', type=int, default=5, required=False,
@@ -23,6 +25,7 @@ read1 = results.read1
 read2 = results.read2
 project = results.project
 o = results.o
+gff = results.gff
 reference = results.reference
 
-smaps(read1, project, o, read2, reference)
+smaps(read1, project, o, read2, reference, gff)
